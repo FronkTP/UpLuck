7bf384001e4063821ee2dd055cc4e3984d05e262
FronkTP <96272954+FronkTP@users.noreply.github.com>
2025-03-13 20:46:47 +0700

    edit colour palette

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="hero-section">
             <img src={nikon} alt="surprised nikon" className="hero__image"/>
-            <div className="hero__div">
+            <div className="hero__text">
                 <h1>UpLuck</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 <button className="hero__btn"><a href="">Start your do duang journey!</a></button>
