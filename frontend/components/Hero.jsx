cd69042d5e32b06a02a57b55baa8cfb1ae182677
FronkTP <96272954+FronkTP@users.noreply.github.com>
2025-03-16 23:17:40 +0700

    add a navigation bar

---
+++ frontend/components/Hero.jsx
@@ -0,0 +1,14 @@
+import nikon from "../src/images/surprised_nikon.png"
+
+export default function Hero() {
+    return (
+        <header className="hero-section">
+            <img src={nikon} alt="surprised nikon" className="hero__image"/>
+            <div className="hero__text">
+                <h1>UpLuck</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <button className="hero__btn"><a href="">Start your do duang journey!</a></button>
+            </div>
+        </header>
+    )
+}
