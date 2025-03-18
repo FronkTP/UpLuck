7221340d6c64b432f691abf387d5fdf27047e1f3
GuyP <guyparnchinda@gmail.com>
2025-03-18 23:52:56 +0700

    embeb map inside sacredmap

--- frontend/components/SacredMap.jsx
+++ frontend/components/SacredMap.jsx
@@ -3,6 +3,7 @@ export default function SacredMap() {
         <div>
             <h2>Sacred Map</h2>
             <p>Explore places.</p>
+            <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1GkVZF9m96WK6bgK_PSI_ZEutow2IHMc&ehbc=2E312F&noprof=1" width="640" height="480"></iframe>
         </div>
     );
 }
