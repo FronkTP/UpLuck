04592abcc55a7ec7f03c3a6faf79bb62ed784b8e
FronkTP <96272954+FronkTP@users.noreply.github.com>
2025-03-20 01:54:27 +0700

    edit styles of about us page

--- frontend/pages/Aboutus.jsx
+++ frontend/pages/Aboutus.jsx
@@ -6,27 +6,30 @@ export default function Aboutus(){
     return (
         <>
             <Navbar/>
-            <h1 class="questions">Who are we?</h1>
-            <div class="answers">
-            <p>
-                We are a team of sick(six) ISE students in the ICE program who love coding(but almost got an F in computer programming) and designing. 
-            We have created this website to UpLuck(our website’s name)  for you. This website provides you with the sources of fortune telling and increasing your luck. 
-            Hope you enjoy our website and have a great day!
-            </p>
+            <div className="main">
+                <h1 class="questions">Who are we?</h1>
+                <div class="answers">
+                <p>
+                    We are a team of sick(six) ISE students in the ICE program who love coding(but almost got an F in computer programming) and designing. 
+                We have created this website to UpLuck(our website’s name)  for you. This website provides you with the sources of fortune telling and increasing your luck. 
+                Hope you enjoy our website and have a great day!
+                </p>
+                </div>
+                
+                <h1 class="questions">How do we tell your fortune?</h1>
+                <div class="answers">
+                <p>
+                    We use a super secret recipe made by our great-great-great grandparents that has been passed down to us.
+                    If you are by chance interested in our secret recipe, give us pi baht, with infinite decimal places accuracy. 
+                    If you can't, then we have to keep it a secret, sadly.
+                    <br></br>
+                    Nevertheless, we gaurantee that Upluck will up your luck and smoothen your uni's life!
+                </p>
+                </div>
+
+                <img src={absolute_cinema} alt="peak" class="peak_img"/>
             </div>
             
-            <h1 class="questions">How do we tell your fortune?</h1>
-            <div class="answers">
-            <p>
-                We use a super secret recipe made by our great-great-great grandparents that has been passed down to us.
-                If you are by chance interested in our secret recipe, give us pi baht, with infinite decimal places accuracy. 
-                If you can't, then we have to keep it a secret, sadly.<br></br>
-                <br></br>
-                Nevertheless, we gaurantee that Upluck will up your luck and smoothen your uni's life!
-            </p>
-            </div>
-
-            <img src={absolute_cinema} alt="peak" class="peak_img"/>
         </>
     )
     
