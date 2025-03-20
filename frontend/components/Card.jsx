e2f766bd973c4be96f0c53dcef1ea4d4627ef796
FronkTP <96272954+FronkTP@users.noreply.github.com>
2025-03-20 14:39:55 +0700

    edit styles for each card component

--- frontend/components/Card.jsx
+++ frontend/components/Card.jsx
@@ -4,11 +4,9 @@ export default function Card(props){
             <div className={`card_main ${props.isSelected ? 'selected' : ''}`}>
                 <div className="card_margin">
                     <img src={props.img} className="card_img"/>
-                    <div className="card_info">
-                        <div className="card_text">
+                    <div className="card_text">
                             <h2 className="card_title">{props.title}</h2>
                             <p className="card_desc">{props.desc}</p>
-                        </div>
                     </div>
                 </div>
             </div>
